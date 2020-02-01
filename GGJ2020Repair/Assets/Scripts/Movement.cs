f7ccbeec2f1a919ca38da3e0eb9e1b8713f9b10d
Buscems <46265895+Buscems@users.noreply.github.com>
2020-01-31 21:53:23 -0500

    Yeah?

---
+++ GGJ2020Repair/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    public Vector2 input;
+    public bool p1, p2;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (p1)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            rb.MovePosition(rb.position + input * speed * Time.deltaTime);
+        }
+        if(p2){
+            input.x = Input.GetAxisRaw("Horizontal2");
+            input.y = Input.GetAxisRaw("Vertical2");
+            rb.MovePosition(rb.position + input * speed * Time.deltaTime);
+        }
+	}
+}
