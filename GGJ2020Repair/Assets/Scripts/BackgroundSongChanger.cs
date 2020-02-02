6c7aa956a3705e68c25b21dd312bc796e7fe21a9
Backsauce <gk.42@hotmail.com>
2020-02-02 09:30:56 -0500

    added song changer and stuff

---
+++ GGJ2020Repair/Assets/Scripts/BackgroundSongChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSongChanger : MonoBehaviour {
+
+    public AudioSource audioSource1, audioSource2;
+    public float waterThreshold;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Spawner.amountOfWaterParticles >= waterThreshold)
+        {
+            audioSource1.volume = 0;
+            audioSource2.volume = 1;
+        } else {
+            audioSource1.volume = 1;
+            audioSource2.volume = 0;
+        }
+	}
+}
